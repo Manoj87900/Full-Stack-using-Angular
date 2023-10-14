ee70b1b9c6910e5a64d6c8b8de20858f3b1ac6a4
manoj87900 <manojsannapaneni@gmail.com>
2023-10-13 17:31:35 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+searchEmployees(arg0: any) {
+throw new Error('Method not implemented.');
+}
+onOpenModal(arg0: null,arg1: string) {
+throw new Error('Method not implemented.');
+}
+  public employees: Employee[] | undefined; 
+
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit(){
+    this.getEmployees();
+  }
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+        console.log(this.employees);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+
+    );
+  }
+ 
 }
